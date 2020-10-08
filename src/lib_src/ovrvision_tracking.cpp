319b55f1b9eab38432eb351e4a790c1b9ecdb276
kuronekodaisuki <pipomaomy2005@gmail.com>
2020-10-09 01:11:24 +0900

    OpenCV3.0.0依存部分削除

--- src/lib_src/ovrvision_tracking.cpp
+++ src/lib_src/ovrvision_tracking.cpp
@@ -24,31 +24,6 @@
 #pragma warning(default : 4819)
 
 //for cv::imshow
-#ifdef _DEBUG
-#pragma comment(lib, "Comctl32.lib")
-#pragma comment(lib, "IlmImfd.lib")
-#pragma comment(lib, "ippicvmt.lib")
-#pragma comment(lib, "libjpegd.lib")
-#pragma comment(lib, "libpngd.lib")
-#pragma comment(lib, "libtiffd.lib")
-#pragma comment(lib, "libwebpd.lib")
-#pragma comment(lib, "libjasperd.lib")
-#pragma comment(lib, "strmiids.lib")
-#pragma comment(lib, "zlibd.lib")
-#pragma comment(lib, "opencv_highgui300d.lib")
-#else
-#pragma comment(lib, "Comctl32.lib")
-#pragma comment(lib, "IlmImf.lib")
-#pragma comment(lib, "ippicvmt.lib")
-#pragma comment(lib, "libjpeg.lib")
-#pragma comment(lib, "libpng.lib")
-#pragma comment(lib, "libtiff.lib")
-#pragma comment(lib, "libwebp.lib")
-#pragma comment(lib, "libjasper.lib")
-#pragma comment(lib, "strmiids.lib")
-#pragma comment(lib, "zlib.lib")
-#pragma comment(lib, "opencv_highgui300.lib")
-#endif
 //#define OV_CONFIG_USEOPENCL
 
 #include "ovrvision_handimage.h"
